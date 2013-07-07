6862392065b06a8cf66c24c953423ed37a3eb9de
Alain Adler <alain.adler@gmail.com>
2013-07-07 06:26:59 +0000

    fixed classpath and added dial example

---
+++ src/foneapijavawrapper/examples/DialCallExample.java
@@ -0,0 +1,24 @@
+package foneapijavawrapper.examples;
+
+import foneapijavawrapper.commands.*;
+
+public class DialCallExample {
+
+	public static void main(String[] args) {
+		FoneApiClient client = new FoneApiClient(
+				"http://foneapi_server/api/1.0/switch",
+				"****************************",
+				"****************************");
+		CommandResponse response = client.dial(eDialDestinationType.number,
+				"5551234567",
+				"htt://your_server/location_of_call_answered_event_handler", 0,
+				"htt://your_server/location_of_error_event_handler", 30,
+				"5557654321", "Lester Tester", 0, "verizon");
+		if (response.getStatus() != 0) {
+			System.out.println("Call failed with error: "
+					+ response.getErrorMsg());
+		} else {
+			System.out.println("Call id: " + response.getCallId());
+		}
+	}
+}
